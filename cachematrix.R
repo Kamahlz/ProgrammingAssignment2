e19c792564b2d23f6be1ce94c254d51b83deec00
Kamahlz <rodrigo.zigante@gmail.com>
2015-03-22 14:14:04 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,26 +5,26 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
- m <- NULL
-  set <- function(y) {
+ m <- NULL   ## Start defining m as NULL, ie initialize
+  set <- function(y) {   ##  Take the new matrix and set it to x
     x <<- y
-    m <<- NULL
+    m <<- NULL           ## Set m as NULL in the global environment
   }
-  get <- function() x
-  setsolve <- function(solve) m <<- solve
+  get <- function() x ## Get the value in the evaluating environment
+  setsolve <- function(solve) m <<- solve ## Set the value of solve in parent environment for the variable m
   getsolve <- function() m
-  list(set = set, get = get,
+  list(set = set, get = get, 
        setsolve = setsolve,
-       getsolve = getsolve)
-
+       getsolve = getsolve)    ## Here we list the value of the functions that will be used in cacheSolve
+ 
 }
 
 cacheSolve <- function(x, ...) {
 
-m <- x$getsolve()
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setsolve(m)
+m <- x$getsolve()   ## Retrive and assign the old cache value
+  data <- x$get()   ## If x it's not in the cache (wasn't evaluated before) assign x matrix to data
+  m <- solve(data, ...) ## Calculate the inverse of the matrix
+  x$setsolve(m)    ## Assign the inverse of the matrix to the m environment
   m
   
   
